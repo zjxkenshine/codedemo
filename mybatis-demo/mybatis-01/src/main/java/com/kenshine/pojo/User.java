01d94ba2c2352324751fcb0162606a7c0943eec4
kenshine <1754294529@qq.com>
2020-11-05 10:36:33 +0800

    修改

--- mybatis-demo/mybatis-01/src/main/java/com/kenshine/pojo/User.java
+++ mybatis-demo/mybatis-01/src/main/java/com/kenshine/pojo/User.java
@@ -1,16 +1,12 @@
 package com.kenshine.pojo;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * @author kenshine
  * @create 2020-10-30 15:50
  **/
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class User {
     private int id;
     private String name;
