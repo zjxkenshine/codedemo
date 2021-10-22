e155f0712abf0cfb06412205470aef66b9683802
Kenshine <1754294529@qq.com>
2021-10-22 22:08:08 +0800

    黑马——中介者模式

---
+++ design-pattern-demo/hm-design-pattern-demo/src/main/java/com/kenshine/pattern3/mediator/Mediator.java
@@ -0,0 +1,12 @@
+package com.kenshine.pattern3.mediator;
+
+/**
+ * @version v1.0
+ * @ClassName: Mediator
+ * @Description: 抽象中介者类
+ * @Author: kenshine
+ */
+public abstract class Mediator {
+
+    public abstract void constact(String message,Person person);
+}
