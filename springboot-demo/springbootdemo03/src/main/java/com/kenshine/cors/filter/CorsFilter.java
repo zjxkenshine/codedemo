2144a91fa8a16d9507c421e180067d85da4e1c9b
kenshine <1754294529@qq.com>
2021-11-05 09:55:37 +0800

    跨域补充

--- springboot-demo/springbootdemo03/src/main/java/com/kenshine/cors/filter/CorsFilter.java
+++ springboot-demo/springbootdemo03/src/main/java/com/kenshine/cors/filter/CorsFilter.java
@@ -1,9 +1,12 @@
 package com.kenshine.cors.filter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -31,9 +34,18 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin","*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
+        //允许的请求头
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        //手动处理预检请求
+        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        // 跨域时会首先发送一个option请求，直接返回正常状态
+        if (request.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            response.setStatus(HttpStatus.OK.value());
+        }
+
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
