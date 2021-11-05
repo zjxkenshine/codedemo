1a157b86169e5b99df89417cb429bd629778956a
kenshine <1754294529@qq.com>
2021-11-05 12:06:43 +0800

    跨域补充

--- springboot-demo/springbootdemo03/src/main/java/com/kenshine/cors/filter/CorsFilter.java
+++ springboot-demo/springbootdemo03/src/main/java/com/kenshine/cors/filter/CorsFilter.java
@@ -1,6 +1,7 @@
 package com.kenshine.cors.filter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,9 +25,11 @@ import java.io.IOException;
  * 基于过滤器的实现方案
  * @CrossOrigin注解实现方案
  *
+ * 注意：nginx配置中有一项 underscores_in_headers on; #该属性默认为off，表示如果header name中包含下划线，则忽略掉。
  */
 @Configuration
-@WebFilter(filterName = "CorsFilter")
+@Order(1)
+@WebFilter(filterName = "CorsFilter",urlPatterns = "/**")
 public class CorsFilter implements Filter {
 
     @Override
@@ -34,10 +37,16 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin","*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT, OPTIONS");
+        response.setHeader("Access-Control-Allow-Methods", "POST,GET,PATCH,DELETE,PUT,OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        //允许的请求头
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        //允许的请求头 自定义请求头添加到这里
+        response.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+
+        /**
+         *  httpServletResponse.setHeader("Access-control-Allow-Origin", httpServletRequest.getHeader("Origin"));
+         *  httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
+         *  httpServletResponse.setHeader("Access-Control-Allow-Headers", httpServletRequest.getHeader("Access-Control-Request-Headers"));
+         */
 
         //手动处理预检请求
         HttpServletRequest request= (HttpServletRequest) servletRequest;
