0466fdb2b76c3b0e429bf57976533034af074b02
kenshine <1754294529@qq.com>
2021-12-14 10:19:12 +0800

    缺失文件

---
+++ springboot-demos02/springbootdemo178-SpringFactoriesLoader/src/main/java/com/kenshine/factoriesLoader/service/impl/TestServiceImpl.java
@@ -0,0 +1,16 @@
+package com.kenshine.factoriesLoader.service.impl;
+
+import com.kenshine.factoriesLoader.service.TestService;
+
+/**
+ * @author ：kenshine
+ * @date ：Created in 2021/12/14 10:11
+ * @description：测试
+ * @modified By：
+ * @version: $
+ */
+public class TestServiceImpl implements TestService {
+    public TestServiceImpl(){
+        System.out.println("TestServiceImpl被实例化了");
+    }
+}
