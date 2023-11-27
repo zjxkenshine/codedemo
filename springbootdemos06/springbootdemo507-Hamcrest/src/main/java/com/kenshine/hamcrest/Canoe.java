af86dafdff5f41c3c15681689bb41fe139cb15ef
kenshine <1754294529@qq.com>
2023-11-27 16:48:21 +0800

    Hamcrest 匹配器

--- springbootdemos06/springbootdemo507-Hamcrest/src/main/java/com/kenshine/hamcrest/Canoe.java
+++ springbootdemos06/springbootdemo507-Hamcrest/src/main/java/com/kenshine/hamcrest/Canoe.java
@@ -3,7 +3,7 @@ package com.kenshine.hamcrest;
 /**
  * @author by kenshine
  * @Classname Canoe
- * @Description TODO
+ * @Description 测试类
  * @Date 2023-11-27 16:44
  * @modified By：
  * @version: 1.0$
